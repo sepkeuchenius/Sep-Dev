394e9e96149ddd4b4612761efcdfb217ed5d3167
sepkeuchenius <septimber@gmail.com>
2021-01-12 12:18:57 +0100

    text

--- js/calc.js
+++ js/calc.js
@@ -272,7 +272,11 @@
     $newDiv = $('<div>')
     $newDiv.attr('class', $('#type').val())
     console.log($('#type').val())
-    $newDiv.text($('#name').val())
+    // $newDiv.text($('#name').val())
+    var textPart = $('<div>')
+    textPart.attr('class', 'text')
+    textPart.text($('#name').val())
+    $newDiv.append(textPart);
     $newDiv.attr('id', idN++);
     console.log($('#colorpicker .selected').attr('id'))
     $newDiv.css('background-color', colors[$('#colorpicker .selected').attr('id')]);
