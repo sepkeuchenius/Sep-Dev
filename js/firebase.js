725cadad621b74469434bcba3fa7bacad67de4bb
sepkeuchenius <septimber@gmail.com>
2021-03-26 12:16:15 +0100

    no google

--- js/firebase.js
+++ js/firebase.js
@@ -18,7 +18,6 @@ $(document).ready(function(){
     signInFlow: 'popup',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
 
